a00bc8784cd42ede898e230fb145b97a41b32ce3
narin12 <123357243+narin12@users.noreply.github.com>
2026-08-26 09:48:07 +0900

    Change bot status from '웅나 웅나' to 'BABO'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "웅나 웅나",     // this is what people actually see
+        state: "BABO",     // this is what people actually see
         type: 0,               // Custom
       },
     ],
